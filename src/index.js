961c55c7ddc1c9d3e3a70804bd8cbebe1f31a9bc
wyatt-h <wyatt.hskr@gmail.com>
2022-05-18 23:36:10 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import reactDOM from 'react-dom'
+import "./styles/style.css"
+
+const App = () => {
+    return <h1>Hello from react</h1>
+}
+
+reactDOM.render(<App />, document.getElementById("root"))
